c29fea2086cc460f76600ff49044bb995546d9de
ervinpepic <ervin.hack@gmail.com>
2018-01-12 16:31:36 +0100

    Added Ability to fin user location and center in radius 100 meters around, give user nice button to locate when in move

--- map-town/Controller/MapVC.swift
+++ map-town/Controller/MapVC.swift
@@ -8,24 +8,52 @@
 
 import UIKit
 import MapKit
+import CoreLocation
 
 class MapVC: UIViewController {
 
     @IBOutlet weak var mapView: MKMapView!
     
+    let authStatus = CLLocationManager.authorizationStatus()
+    let regionRadius: Double = 1000
+    var locationManager = CLLocationManager()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         mapView.delegate = self
+        locationManager.delegate = self
+        configLocationServices()
      
     }
 
 
     @IBAction func getLocationButton(_ sender: Any) {
+        if authStatus == .authorizedAlways || authStatus == .authorizedWhenInUse {
+            centerMapOnCurrentUserLocation()
+        }
     }
     
 }
 
 
 extension MapVC: MKMapViewDelegate {
-    
+    func centerMapOnCurrentUserLocation() {
+        guard let coordinate = locationManager.location?.coordinate else { return }
+        let coordinateRegion = MKCoordinateRegionMakeWithDistance(coordinate, regionRadius * 2.0 , regionRadius * 2.0)
+        mapView.setRegion(coordinateRegion, animated: true)
+    }
+}
+
+extension MapVC: CLLocationManagerDelegate {
+    func configLocationServices() {
+        if authStatus == .notDetermined {
+            locationManager.requestAlwaysAuthorization()
+        } else {
+            return
+        }
+    }
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        centerMapOnCurrentUserLocation()
+    }
 }
