cd8d330b8603b0eaa073741501fa765222802b54
ervinpepic <ervin.hack@gmail.com>
2018-01-12 16:05:19 +0100

    Created User interface and iboutlets and actions

--- map-town/Controller/MapVC.swift
+++ map-town/Controller/MapVC.swift
@@ -7,20 +7,25 @@
 //
 
 import UIKit
-
+import MapKit
 
 class MapVC: UIViewController {
 
+    @IBOutlet weak var mapView: MKMapView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        mapView.delegate = self
+     
     }
 
 
+    @IBAction func getLocationButton(_ sender: Any) {
+    }
+    
 }
 
+
+extension MapVC: MKMapViewDelegate {
+    
+}
